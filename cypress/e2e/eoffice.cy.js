f32f0b6e62a0eb0ff9d9b40220a3aa621e207a32
Gafiliano Gapili <gafiliano@gmail.com>
2025-01-16 20:35:13 +0700

    feat(eoffice): add kehadiran flow

--- cypress/e2e/eoffice.cy.js
+++ cypress/e2e/eoffice.cy.js
@@ -1,5 +1,25 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+import * as loginPage from "../pages/LoginPage"
+import * as eoffice from "../pages/EofficePage"
+
+describe('E-Office Auto-present', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('User do create kehadiran', () => {
+    loginPage.setCredentials(Cypress.env('NPP'), Cypress.env('PASSWORD'))
+    loginPage.clickBtnLogin()
+    loginPage.navigateToAbsensiMenu()
+    eoffice.navigateToMenu('Hadir')
+    eoffice.fillKehadiran(
+      Cypress.env('CLOCK_OUT'), 
+      Cypress.env('CLOCK_IN'), 
+      Cypress.env('DATE_DIFF'),
+      Cypress.env('DEFAULT_REASON'), 
+      Cypress.env('DEFAULT_WITNESS'), 
+      Cypress.env('DEFAULT_COMMENT')
+    )
+    cy.wait(1000)
+    eoffice.logout()
   })
 })
