18ab0157b6651fea308c9f295f1e2dbca1213dc6
Gafiliano Gapili <gafiliano.gapili@bni.co.id>
2025-01-17 08:14:00 +0700

    update: introduce package cypress-dotenv

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+const { dotenvPlugin } = require('cypress-dotenv');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://e-office.bni.co.id',
+    setupNodeEvents(on, config) {
+      const updateConfig = dotenvPlugin(config, null, true)
+      return updateConfig
+    },
+  },
+  watchForFileChanges: false,
+});
