0d1aac29911a14b8f068453896c69068a785ea38
Gafiliano Gapili <gafiliano@gmail.com>
2025-01-30 11:22:09 +0700

    update: test other var

--- cypress/e2e/eoffice.cy.js
+++ cypress/e2e/eoffice.cy.js
@@ -11,17 +11,17 @@ describe('E-Office Auto-present', () => {
     loginPage.clickBtnLogin()
     loginPage.navigateToAbsensiMenu()
     eoffice.navigateToMenu('Hadir')
-    // eoffice.fillKehadiran(
-    //   Cypress.env('CLOCK_OUT'),
-    //   Cypress.env('CLOCK_IN'),
-    //   Number(Cypress.env('DATE_DIFF')),
-    //   Cypress.env('REASON'),
-    //   Cypress.env('WITNESS'),
-    //   Cypress.env('REVIEWER1'),
-    //   Cypress.env('REVIEWER2'),
-    //   Cypress.env('IS_SUBMIT'),
-    //   String(Cypress.env('COMMENT'))
-    // )
+    eoffice.fillKehadiran(
+      Cypress.env('CLOCK_OUT'),
+      Cypress.env('CLOCK_IN'),
+      Number(Cypress.env('DATE_DIFF')),
+      Cypress.env('REASON'),
+      Cypress.env('WITNESS'),
+      Cypress.env('REVIEWER1'),
+      Cypress.env('REVIEWER2'),
+      Cypress.env('IS_SUBMIT'),
+      String(Cypress.env('COMMENT'))
+    )
     cy.wait(1000)
     eoffice.logout()
   })
